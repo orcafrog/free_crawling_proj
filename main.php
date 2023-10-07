bf172fad3956a62dea994a930832622ccd4141cd
orcafrog <orca.of@gmail.com>
2023-10-07 21:21:19 +0900

    first commit

---
+++ main.php
@@ -0,0 +1,16 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>main_page</title>
+  </head>
+  <body>
+    <a>이곳은 main_page 입니다.</a><br>
+    <a>로그인을 하셔야 페이지를 보실 수 있습니다.</a>
+    <a>로그인을 원하시면</a><br>
+    <a>아래 버튼을 눌러주세요.</a><br>
+    <button type="button" class="navyBtn" onClick="location.href='../index/check_user/pr_login/pr_login.html'">click here</button>
+  </body>
+</html>
+<?php
+
+?>
